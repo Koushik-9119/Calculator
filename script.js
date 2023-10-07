83c822ade43ae80f7b1bf8ac52f641135227192f
Koushik <m.koushik.ias.cs@gmail.com>
2023-10-07 17:53:54 +0530

    files are added

---
+++ script.js
@@ -0,0 +1,27 @@
+function sinclicked(){
+    form.display.value = Math.sin(form.display.value)
+}
+function cosclicked(){
+    form.display.value = Math.cos(form.display.value)
+}
+function tanclicked(){
+    form.display.value = Math.cos(form.display.value)
+}
+function sqrtclicked(){
+    form.display.value = Math.sqrt(form.display.value)
+}
+function logclicked(){
+    form.display.value = Math.log(form.display.value)
+}
+function squareclicked(){
+    form.display.value = Math.pow(form.display.value)
+}
+function coshclicked(){
+    form.display.value = Math.cosh(form.display.value)
+}
+function sinhclicked(){
+    form.display.value = Math.sinh(form.display.value)
+}
+function tanhclicked(){
+    form.display.value = Math.tanh(form.display.value)
+}
